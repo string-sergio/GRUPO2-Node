9697b3322a67dd382f60173c06f0bf3c92f435d5
string-sergio <alu0101130330@ull.edu.es>
2021-11-10 18:56:08 +0000

    Página 37 terminada

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-console.log("loaded")
+//PAGINA 36
+
+console.log("loaded ")
 
 const carasDado = Math.floor(100*Math.random())+1
 
 const dado = Math.floor(carasDado*Math.random())+1;
 
 console.log(dado)
+
+//PAGINA 37
+
+let palabras_counter = 0
+const frase = "aAa"
+
+for (let index = 0; index < frase.length; index++) {
+    if (frase[index].toLowerCase() === "a") {
+        palabras_counter++
+    }
+}
+console.log(palabras_counter)
