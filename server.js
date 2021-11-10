4813de9581f62b4ad47ca5ba175215e3c26eab66
string-sergio <alu0101130330@ull.edu.es>
2021-11-10 19:09:57 +0000

    Página 42 terminada

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 console.log("loaded ")
 
-const carasDado = Math.floor(100*Math.random())+1
+const carasDado = Math.floor(100 * Math.random()) + 1
 
-const dado = Math.floor(carasDado*Math.random())+1;
+const dado = Math.floor(carasDado * Math.random()) + 1;
 
 console.log(dado)
 
@@ -19,3 +19,32 @@ for (let index = 0; index < frase.length; index++) {
     }
 }
 console.log(palabras_counter)
+
+//PAGINA 42
+
+const fs = require('fs');
+
+const filePath = 'TEXTO.txt';
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+
+    if (err) {
+
+        console.error('Error', err);
+
+    } else {
+
+        data.toString();
+        let word_file_counter = 1
+        if (data === "") {
+            word_file_counter = 0
+        }
+        for (let index = 0; index < data.length; index++) {
+            if (data[index].toLowerCase() === " ") {
+                word_file_counter++
+            }
+        }
+        console.log(word_file_counter)
+    }
+
+});
